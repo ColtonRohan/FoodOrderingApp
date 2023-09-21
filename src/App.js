23cb625a13ac4c037154c2273d8253c61167120d
ColtonRohan <crohan2020@fau.edu>
2023-09-20 21:54:19 -0400

    testing commit
    commiting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
 import CartProvider from "./store/CartProvider";
-
+//test
 function App() {
   const [showModal, setShowModal] = useState(false);
 
